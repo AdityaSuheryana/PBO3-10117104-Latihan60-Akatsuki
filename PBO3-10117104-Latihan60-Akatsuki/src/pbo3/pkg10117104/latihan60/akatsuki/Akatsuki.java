1ff58a9278729997f0e2ca202f3d8c928c78a5f4
AdityaSuheryana <43383601+AdityaSuheryana@users.noreply.github.com>
2018-10-30 01:14:49 +0700

    PBO3-10117104-Latihan60-Akatsuki

---
+++ PBO3-10117104-Latihan60-Akatsuki/src/pbo3/pkg10117104/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan60.akatsuki;
+
+/**
+ *
+ * @author user
+ */
+public class Akatsuki {
+
+private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+   public void tampil(){
+       
+   } 
+}
---
+++ PBO3-10117104-Latihan60-Akatsuki/src/pbo3/pkg10117104/latihan60/akatsuki/Desa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan60.akatsuki;
+
+/**
+ *
+ * @author user
+ */
+public class Desa extends Akatsuki{
+
+private String Desa;
+
+    public String getDesa() {
+        return Desa;
+    }
+
+    public void setDesa(String Desa) {
+        this.Desa = Desa;
+    }
+
+    @Override
+    public void tampil() {
+        
+    }
+
+    
+}
